c21c3a8383715f2bb99e6d57d1a962a92183132d
Latricia Hunter <latricia@Latricias-iMac.local>
2022-05-16 12:58:03 -0700

    created beginner server

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // write your code here
+const http = require('http')
+
+const server= http.createServer(function (req, res) {
+//msg logged when reqest is received
+//console.log('Hello World');
+
+// send headers
+res.writeHead(200, {'Content-Type': "text/plain"});
+
+
+//send the body respose
+res.end('Hello World');
+});
+
+// listen for incomming connections
+server.listen(8080, 'localhost', null, function(){
+
+    //log msg that the server is listening and which port
+    console.log('Server is listening on localhost:8080');
+});
